5d7315d66c1ef022b4e50f5a5ef24af255632373
Artur <a.blajda17@gmail.com>
2023-06-08 12:48:11 +0200

    Update

---
+++ src/js/main.js
@@ -0,0 +1,136 @@
+/*=================== POCZĄTEK SLIDERA ===================*/
+// Zdefiniowanie indeksu bieżącego slajdu
+let currentSlide = 0
+
+// Pobranie wszystkich elementów slajdów
+const slides = Array.from(document.querySelectorAll('.slide'))
+
+// Pobranie elementu kontenera slajdów
+const slider = document.querySelector('.slider')
+
+// Funkcja do zmiany slajdu
+function changeSlide(index) {
+	// Wyliczenie wartości przesunięcia na podstawie indeksu slajdu
+	const slideWidth = slides[0].offsetWidth
+	const displacement = -1 * slideWidth * index
+
+	// Przesunięcie kontenera slajdów
+	slider.style.transform = `translateX(${displacement}px)`
+
+	// Aktualizacja indeksu bieżącego slajdu
+	currentSlide = index
+}
+
+// Funkcja do automatycznego przełączania slajdów co 5 sekund
+function autoSlide() {
+	const totalSlides = slides.length
+	const nextSlide = (currentSlide + 1) % totalSlides
+
+	changeSlide(nextSlide)
+}
+
+// Ustawienie interwału do automatycznego przełączania slajdów
+setInterval(autoSlide, 5000)
+/*=================== KONIEC SLIDERA ===================*/
+
+/* ================== KATEGORIE JEDZENIA  POCZĄTEK==============*/
+document.addEventListener('DOMContentLoaded', () => {
+	const categories = document.querySelectorAll('.category')
+	const itemContainer = document.querySelector('.item-container')
+
+	categories.forEach(category => {
+		category.addEventListener('click', () => {
+			const selectedCategory = category.dataset.category
+
+			categories.forEach(category => {
+				category.classList.remove('active')
+			})
+
+			category.classList.add('active')
+
+			const items = document.querySelectorAll('.item')
+			items.forEach(item => {
+				item.style.display = 'none'
+			})
+
+			const selectedItems = document.querySelectorAll(`.item[data-category="${selectedCategory}"]`)
+			selectedItems.forEach((item, index) => {
+				item.style.display = 'flex'
+				item.style.animationDelay = `${index * 0.1}s`
+				item.classList.add('animate-item')
+			})
+
+			itemContainer.style.display = 'flex'
+		})
+	})
+
+	// Select the initial category
+	const initialCategory = document.querySelector('.category[data-category="pizza"]')
+	initialCategory.click()
+
+	// Add animation class to items in the initial category after page load
+	const initialItems = document.querySelectorAll('.item[data-category="pizza"]')
+	initialItems.forEach((item, index) => {
+		item.style.animationDelay = `${index * 0.1}s`
+		item.classList.add('animate-item')
+	})
+})
+
+/* ================== KATEGORIE JEDZENIA  KONIEC==============*/
+
+/* =========== SCROLLOWANIE NAGŁÓWKA ============*/
+window.addEventListener('scroll', function () {
+	const header = document.querySelector('header')
+	header.classList.toggle('scrolled', window.scrollY > 0)
+})
+
+$(document).ready(function () {
+	$('a[href^="#"]').on('click', function (event) {
+		var target = $(this.getAttribute('href'))
+		if (target.length) {
+			event.preventDefault()
+			$('html, body').stop().animate(
+				{
+					scrollTop: target.offset().top,
+				},
+				800
+			) // Czas trwania animacji w milisekundach
+		}
+	})
+})
+/* =========== SCROLLOWANIE NAGŁÓWKA KONIEC============*/
+
+/* ========= ANIMACJE POJAWIANIA SIĘ ELEMENTÓW ===========*/
+document.addEventListener('DOMContentLoaded', function () {
+	AOS.init({
+		duration: 1000, // Czas trwania animacji (w milisekundach)
+		delay: 200, // Opóźnienie animacji (w milisekundach)
+		once: true, // Animacja zostanie odtworzona tylko raz
+		easing: 'ease-out', // Krzywa animacji
+	})
+
+	// Znajdujemy elementy z klasą "type-effect"
+	var typeElements = document.querySelectorAll('.type-effect')
+
+	// Dla każdego elementu ustawiamy animację pisania się
+	typeElements.forEach(function (element) {
+		element.innerHTML = '' // Usuwamy oryginalną zawartość elementu
+		var text = element.dataset.aosText // Pobieramy tekst z atrybutu "data-aos-text"
+		var delay = parseInt(element.dataset.aosDelay) // Pobieramy opóźnienie z atrybutu "data-aos-delay"
+		var duration = parseInt(element.dataset.aosDuration) // Pobieramy czas trwania z atrybutu "data-aos-duration"
+		var charIndex = 0 // Inicjalizujemy indeks znaku
+
+		// Funkcja, która dodaje pojedyncze litery do elementu z efektem pisania
+		function typeWriter() {
+			if (charIndex < text.length) {
+				element.innerHTML += text.charAt(charIndex)
+				charIndex++
+				setTimeout(typeWriter, duration)
+			}
+		}
+
+		// Wywołujemy funkcję pisania po opóźnieniu
+		setTimeout(typeWriter, delay)
+	})
+})
+/* ========= ANIMACJE POJAWIANIA SIĘ ELEMENTÓW  KONIEC===========*/
